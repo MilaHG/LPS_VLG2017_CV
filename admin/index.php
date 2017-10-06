e682092a637ca50ecdfd3f0da1b72122951eefce
Kangaroo <mhng75@gmail.com>
2017-10-06 11:20:55 +0200

    'maj'

---
+++ admin/index.php
@@ -0,0 +1,16 @@
+<?php
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+?>
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <meta charset="utf-8">
+        <title>Admin : Prénom Nom</title>
+    </head>
+    <body>
+    </body>
+</html>
